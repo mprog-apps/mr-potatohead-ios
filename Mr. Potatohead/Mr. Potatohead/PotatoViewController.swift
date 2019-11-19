3feeb0d8c876e7423f36151ad0e923928bc31824
emmahokken <emmahokken@gmail.com>
2019-11-19 14:06:36 +0100

    committed code

---
+++ Mr. Potatohead/Mr. Potatohead/PotatoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Mr. Potatohead
+//
+//  Created by Emma Hokken on 18/11/2019.
+//  Copyright © 2019 Emma Hokken. All rights reserved.
+//
+
+import UIKit
+
+class PotatoViewController: UIViewController {
+
+    // Image outlets
+    @IBOutlet weak var bodyImageView: UIImageView!
+    
+    // Switch outlets
+    @IBOutlet weak var armsSwitch: UISwitch!
+    
+    // Label outlets
+    @IBOutlet weak var armsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
